5e4bf02696df5868ef9e2cf70de88955b34d6240
Pablo Berjon <2490772B@student.gla.ac.uk>
2020-04-25 19:42:31 +0100

    Commented a bit. Need to refactor and finish the exam off.

--- src/Exam2018/SystemStudentsGradesAndMatrics.java
+++ src/Exam2018/SystemStudentsGradesAndMatrics.java
@@ -10,30 +10,35 @@ public class SystemStudentsGradesAndMatrics {
 
 	public static void main(String[] args) {
 
+// 		Path to the file and ArrayList to load the students from the file.
 		String path = "C:\\Users\\p_ber\\Workspace\\ProgrammingExams\\src\\Exam2018\\grades.txt";
 		ArrayList<String> document = loadFile(path);
 
+		
+//		Two arrays that will store the matrices and grades of the students.
 		int[] matrics = new int[document.size()];
 		String[] grades = new String[document.size()];
 
+		
+//		Here we pass the information from the ArrayList with the raw information
+//		of the students to the two previous arrays.
 		for (int i = 0; i < document.size(); i++) {
 
 			matrics[i] = Integer.parseInt(document.get(i).substring(0, 7));
 			grades[i] = document.get(i).substring(8);
 
-//			System.out.println("Matric: " + matrics[i] + " Grade: " + grades[i]);
 			String formatted = String.format("Matric: %07d" + " Grade: " + grades[i], matrics[i]);
 			System.out.println(formatted);
 		}
 
+//		Finding the max grade.
 		findMaxGrade(matrics, grades);
 
+//		Now passing the two cumbersome arrays of matrices and grades to a better array of object student
 		StudentGrade[] students = new StudentGrade[matrics.length];
 		for (int j = 0; j < matrics.length; j++) {
 			students[j] = new StudentGrade(matrics[j], grades[j]);
 		}
-		
-//		java.util.Arrays.sort(students);
 		
 		System.out.println("Sorted");
 		for (int j = 0; j < students.length; j++) {
@@ -41,7 +46,9 @@ public class SystemStudentsGradesAndMatrics {
 		}
 		
 	}
-
+	
+	
+//	The following method retrieves the grades from the students.
 	public static ArrayList<String> loadFile(String path) {
 
 		FileReader filereader;
@@ -67,6 +74,7 @@ public class SystemStudentsGradesAndMatrics {
 		return document;
 	}
 
+//	The following method compares the grades of the students.
 	public static int compareGrades(String gradeOne, String gradeTwo) {
 
 		if ((gradeOne.charAt(0) > gradeTwo.charAt(0)) || (gradeOne.charAt(0) == gradeTwo.charAt(0)) && (gradeOne.charAt(1) > gradeTwo.charAt(1))) {
@@ -82,6 +90,7 @@ public class SystemStudentsGradesAndMatrics {
 
 	}
 
+//	Compares and fins the greatest grade among the students.	
 	public static void findMaxGrade(int[] matrics, String[] grades) {
 
 		String maxGrade = grades[0];
@@ -101,7 +110,7 @@ public class SystemStudentsGradesAndMatrics {
 	
 //	There are several inconveniences when storing data in this way. Firstly, it is hard to manipulate the data,
 //	making it easy to mix information if not handled correctly. Secondly, it is not efficient, as the methods
-//	for manipulating the data need to be more elaborated and to mantain the relation of the matrics with the
-//	grades. A better way would be a hashmap, for instance, or creating an object for students that stores both,
+//	for manipulating the data need to be more elaborated and to maintain the relation of the matrices with the
+//	grades. A better way would be a HashMap, for instance, or creating an object for students that stores both,
 //	the matric and the grades.
 }
