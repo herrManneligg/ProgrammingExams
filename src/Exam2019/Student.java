1519dd0500e67fc8d6b844a1223526862aa233a6
Pablo Berjon <2490772B@student.gla.ac.uk>
2020-04-26 14:47:15 +0100

    Finished the exam from 2019. I wrote a bit in the writing questions, although I think that better resources can be found online or in books in this regard. I did not finish the last question because of the same reason. The questions are rather easy either way.

--- src/Exam2019/Student.java
+++ src/Exam2019/Student.java
@@ -59,7 +59,37 @@ public class Student {
 				PGStudent a = new PGStudent("Mary","1234",56.7);
 				Student b = a; // Line D
 				System.out.println(a);
-//				System.out.println(b)
+				System.out.println(b);
+				
+//				At Line D, instead of creating a new object and its reference, we are
+//				creating a new reference that will refer to the object that the reference
+//				"a" is referring to.
+				
+//				Yes, both references would print the same output, as both references are
+//				pointing to the same object.
+				
+//	QUESTION F. Method design for handling a file with the pattern name matric projectgrade.
+//				It will need to handle where a name ends, where a matric starts and ends
+//				and where projectgrade starts. It will probably be based on checking if the
+//				next character is an integer or a string, or loading the file in an ArrayList
+//				as String and then splitting the strings by the spaces, and distributing the
+//				pieces in a student object by making use of String.substring(), or any other
+//				method.
+				
+//			3. 	There are several differences. A GUI program makes use of Java Swing library;
+//				the program has an interface that can be more user friendly, the user does not
+//				need to change the code -whereas a command line may require to make adjustments-,
+//				also, the program in a GUI is waiting to the user to make an action or interact
+//				- usually, although it depends on the specific program -.
+				
+//			4. 	MVC. The Model View Controller design pattern is one of the design patterns used
+//				in programs that make use of user interfaces. The code is split depending on their
+//				tasks; the Model acts as the data storage-database, or also where the program's logic
+//				is kept; the View shows the graphic user interface, and the Controller handles the 
+//				interaction coming from the user, changing the Model accordingly or the View, or
+//				the Model and then the Model changes the View.
+				
+//			5. Interface definition...
 				
 				
 
